fd0cb05bf5dc49e16ab47b6f767a0df880736b9e
Uwe Vogt <uwe.vogt@uv-software.de>
2024-03-29 11:40:45 +0100

    Update MSBuild environment
    Relates to [CANAPI-74]

--- x86_install.bat
+++ x86_install.bat
@@ -2,7 +2,7 @@
 set PWD="%~dp0"
 pushd
 cd /D %PWD%
-copy /Y .\Libraries\CANAPI\Release_dll\uvslcan.dll C:\Windows\SysWOW64
+copy /Y .\Library\Release_dll\uvslcan.dll C:\Windows\SysWOW64
 popd
 dir C:\Windows\SysWOW64\u*can*.dll
 pause
